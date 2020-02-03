06c2d03b1583718b0c82211c17c999a95831012f
Azhang9328 <azhang9328@gmail.com>
2020-02-03 13:59:10 -0800

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,71 +1,86 @@
 document.addEventListener("DOMContentLoaded", function(){
   timerStuff()
   heart()
+  comments()
 })
 
-function timerStuff() {
-    const counter = document.getElementById("counter")
-    let num = parseInt(counter.innerText)
-    let frozen = 0
-    const pp = document.getElementById("pause")
-    
-    function timerStart() {
-        pp.innerText = "pause"
-        if (pp.innerText == "pause") {
-        var interval = setInterval(function() {
-        counter.innerText = num++
-        }, 1000)
-      } else {
-        clearInterval(interval)
-      }
-    }
-
-    function timerPause() {
-    pp.addEventListener("click", function (){
-      if (pp.innerText == "pause") {
-        pp.innerText = "resume"
-      } else {
-        timerStart()
-      }
-    })
-    }
-
-    function timerPlus() {
-        const button = document.getElementById("plus")
-        button.addEventListener("click", function (){
-          counter.innerText = num++
-        })
-      }
-      
-      function timerMinus() {
-        const button = document.getElementById("minus")
-        button.addEventListener("click", function (){
-          counter.innerText = num--
-        })
-      }
-      
-  timerPlus()
-  timerMinus() 
-  timerPause()     
-  timerStart()
+function comments() {
+  let commentForm = document.getElementById("comment-form")
+  let commentsList = document.getElementById("list")
+  commentForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let comment = document.createElement("li")
+    comment.innerText = event.target.comment.value
+    commentsList.appendChild(comment)
+  })
 }
 
+const counter = document.getElementById("counter")
+let num = parseInt(counter.innerText)
+
 function heart() {
-    const heart = document.getElementById("heart")
-    const likeList = document.querySelector("ul")
-    heart.addEventListener("click", function(){
-      let x = 1
-      let likes = document.getElementsByClassName("likes")
-      for (const element of likes) {
-          console.log(parseInt(element.innerText) + "element")
-          console.log(num)
-          if (parseInt(element.innerText) == num) {
-            console.log("true")
-          } 
-      }
+  let numbers = {}
+  const heart = document.getElementById("heart")
+  const likeList = document.querySelector("ul")
+  heart.addEventListener("click", function(){
+    if (!Object.keys(numbers).includes(counter.innerText)) {
+      numbers[`${counter.innerText}`] = 1
       const li = document.createElement("li")
       li.className = 'likes'
-      li.innerHTML = `${num} has been liked ${x} time`
+      li.innerHTML = `${counter.innerText} has been liked ${numbers[`${counter.innerText}`]} time`
       likeList.appendChild(li)
+    } else {
+      let likes = likeList.querySelectorAll("li")
+      for(const like of likes) {
+        if(like.innerText.match(/^[0-9]+/) == counter.innerText) {
+          numbers[`${counter.innerText}`]++
+          like.innerHTML = `${counter.innerText} has been liked ${numbers[`${counter.innerText}`]} times`
+        }
+      }
+    }
+  })
+}
+
+function timerStuff() {
+  let timer = setInterval(timerStart, 1000)
+  const pauseButton = document.getElementById("pause")
+  const plusButton = document.getElementById("plus")
+  const minusButton = document.getElementById("minus")
+
+  function timerStart() {
+    counter.innerText = num++
+  }
+
+  function timerPause() {
+    pauseButton.addEventListener("click", function (){
+      if (pauseButton.innerText == "pause") {
+        clearInterval(timer)
+        plusButton.disabled = true 
+        minusButton.disabled = true
+        pauseButton.innerText = "resume"
+      } else {
+        timer = setInterval(timerStart, 1000)
+        plusButton.disabled = false
+        minusButton.disabled = false
+        pauseButton.innerText = "pause"
+      }
     })
+  }
+
+  function timerPlus() {
+      plusButton.addEventListener("click", function (){
+        counter.innerText = num++
+      })
+    }
+    
+    function timerMinus() {
+      minusButton.addEventListener("click", function (){
+        counter.innerText = num--
+      })
+    }
+    
+timerPause()
+timerPlus()
+timerMinus()
+timerStart()
 }
